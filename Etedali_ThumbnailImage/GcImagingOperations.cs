b56a4ec97293fe3b1953a52570997e92a4c8f086
Mohammad <etedali@gmail.com>
2021-12-11 16:59:55 -0500

    before DynamoDb

---
+++ Etedali_ThumbnailImage/GcImagingOperations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using GrapeCity.Documents.Drawing;
+using GrapeCity.Documents.Text;
+using GrapeCity.Documents.Imaging;
+
+namespace Etedali_ThumbnailImage
+{
+    public class GcImagingOperations
+    {
+        public static string GetConvertedImage(byte[] stream)
+        {
+            using (var bmp = new GcBitmap())
+            {
+                bmp.Load(stream);
+                
+                // Convert to grayscale
+                bmp.ApplyEffect(GrayscaleEffect.Get(GrayscaleStandard.BT601));
+                // Resize to thumbnail
+                var resizedImage = bmp.Resize(256, 256, InterpolationMode.NearestNeighbor);
+                return GetBase64(resizedImage);
+            }
+        }
+
+        #region helper
+        private static string GetBase64(GcBitmap bmp)
+        {
+            using (MemoryStream m = new MemoryStream())
+            {
+                bmp.SaveAsPng(m);
+                return Convert.ToBase64String(m.ToArray());
+            }
+        }
+        #endregion
+    }
+}
